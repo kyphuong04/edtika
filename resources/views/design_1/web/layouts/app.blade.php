cbac81277ab72f5160931e817f8a937420992549
minh10m <minhhh1010@gmail.com>
2026-01-30 10:51:11 +0700

    feat: convert font Gilray to font Plus Jakarta Sans

--- resources/views/design_1/web/layouts/app.blade.php
+++ resources/views/design_1/web/layouts/app.blade.php
@@ -12,6 +12,9 @@
     <title>{{ $pageTitle ?? '' }}{{ !empty($generalSettings['site_name']) ? (' | '.$generalSettings['site_name']) : '' }}</title>
 
     <!-- General CSS File -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/assets/default/vendors/simplebar/simplebar.css">
     <link rel="stylesheet" href="/assets/design_1/css/app.min.css">
 
@@ -36,6 +39,13 @@
         {!! getThemeFontsSettings() !!}
 
         {!! getThemeColorsSettings() !!}
+
+        :root {
+            --main-font-family: 'Plus Jakarta Sans', sans-serif !important;
+        }
+        body, h1, h2, h3, h4, h5, h6, p, a, span, button, input, textarea, select, .btn, div {
+            font-family: 'Plus Jakarta Sans', sans-serif !important;
+        }
     </style>
 
 </head>
